7c86f6c1104371d44e1e9d901392468739215757
GaganNahak <160512461+GaganNahak@users.noreply.github.com>
2024-04-21 22:29:19 +0530

    Add files via upload

---
+++ l.js
@@ -0,0 +1,17 @@
+let yes = document.getElementById( "yes" )
+let no = document.getElementById( "no" )
+let ne = document.createElement( "div" )
+let content = document.getElementById( "content" )
+content.prepend( ne )
+
+
+yes.addEventListener( "click", () => {
+    ne.classList = "ne1"
+    ne.innerText = "🥹Thank you🥹"
+
+} )
+no.addEventListener( "click", () => {
+    ne.classList = "ne2"
+    ne.innerText = "😭Please😭"
+
+} )
